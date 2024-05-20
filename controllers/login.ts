0f5929bfb51ae8b6d93e5836a5eac3897f04b115
Jankowiec <ghjankowiec@gmail.com>
2024-05-20 14:40:04 +0200

    DEL: login comment

--- controllers/login.ts
+++ controllers/login.ts
@@ -7,7 +7,6 @@ export const loginRouter = express.Router();
 loginRouter.post('/login', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const token = await login(req.body);
-        console.log(token)
         res.json({token});
     } catch(e) {
         next(e);
