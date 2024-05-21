55c27d0cad9a2bacfca534364bf240794fbd1839
Jankowiec <ghjankowiec@gmail.com>
2024-05-21 10:38:16 +0200

    ADD: User model

---
+++ models/User.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+import { User } from "../interfaces/User";
+
+const userSchema = new Schema<User>({
+    Picture: String,
+    Full_Name: String,
+    Employee_ID: String,
+    Email: String,
+    Start_Date: String,
+    Description: String,
+    Contact: String,
+    Status: String,
+});
+
+export const UserModel = model('UserModel', userSchema);
