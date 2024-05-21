5433aad696896919bd363f55041387415e5c0f0c
Jankowiec <ghjankowiec@gmail.com>
2024-05-21 10:41:57 +0200

    ADD: Contact model

---
+++ models/Contact.ts
@@ -0,0 +1,14 @@
+import { Schema } from "mongoose";
+import { Contact } from "../interfaces/Contact";
+
+const contactSchema = new Schema<Contact>({
+    date: String,
+    hour: String,
+    message_id: {type: Number, unique: true},
+    subject: String,
+    review: String,
+    picture: String,
+    customer_name: String,
+    mail: String,
+    phone: String,
+});
