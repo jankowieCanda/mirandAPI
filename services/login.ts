09b29ac32bdb7b8ee1e9de88089dd92a98f6747c
Jankowiec <ghjankowiec@gmail.com>
2024-06-07 10:45:04 +0200

    ADD: login service

--- services/login.ts
+++ services/login.ts
@@ -1,25 +1,33 @@
-
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { APIError } from '../APIError';
-import { UserModel } from '../models/User';
+import { connection } from '../myDB';
 const bcrypt = require('bcryptjs')
 
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
-
 export async function login(data: {email: string, password: string}) {
     const {email, password} = data;
-    const user = await UserModel.findOne({Email: email});
-    if(user) {
-        let passOK = bcrypt.compareSync(password, user.Password)
-        if(passOK) {            
-            const userOK = {id: user.Employee_ID, email: user.Email};
-            const token = jwt.sign(userOK, JWT_SECRET);
-            return token;
-        } else {
-            throw new APIError(401, 'Invalid Credential!', true);
+    connection.query(`SELECT * FROM \`user\` WHERE \`Email\` = '${email}' `, (err, rows, fields) => {
+        if (err instanceof Error) {
+          console.log(err);
+          return;
         }
-    }
+        const user = rows[0];
+
+        if(user) {
+            let passOK = bcrypt.compareSync(password, user.Password);
+            if(passOK) {            
+                const userOK = {id: user.Employee_ID, email: user.Email};
+                const token = jwt.sign(userOK, JWT_SECRET);
+                console.log('login token: ' + token)
+                return token;
+            } else {
+                throw new APIError(401, 'Invalid Credential!', true);
+            }
+        }
+        
+    });
+    
 }
